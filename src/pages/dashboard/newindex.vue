c6cd29b5cd2ed5c6dfa3744977ce0fdd42679f88
Caio Abrahão <contato.caioabrahao@gmail.com>
2026-03-05 11:25:06 -0300

    checkpoint: started structuring the new VUE dashboard index

---
+++ src/pages/dashboard/newindex.vue
@@ -0,0 +1,46 @@
+<script>
+import '../../styles/global.css';
+import 'remixicon/fonts/remixicon.css';
+import ImageUpload from '../../components/ImageUpload.astro';
+</script>
+
+<template>
+<section class="h-screen flex flex-col">
+    <div class="w-full px-[5%] p-4">
+        <div class="flex items-center gap-4">
+            <p class="font-gunter text-4xl">CRA</p>
+            <h1 class="text-4xl font-warbler-display font-bold">Dashboard</h1>
+        </div>
+    </div>
+    <div class="px-[5%] grid gap-4 grid-cols-1 grid-rows-3 md:grid-cols-2 lg:grid-cols-3 flex-1 pb-8">
+        <div class="dash-card row-span-3">
+            <h3>Enviar Fotos</h3>
+            <p>Envie fotos para o bucket da Cloudflare</p>
+            <div class="mt-8 p-2 border border-accent rounded-md flex flex-col gap-4">
+                <ImageUpload />
+            </div>
+        </div>
+        <div class="dash-card col-span-2">
+            <h3>Fotos Enviadas</h3>
+            <p>Vejas as fotos existentes no bucket</p>
+        </div>
+        <div class="dash-card col-span-2 row-span-2">
+            <h3>Gerenciar Galerias</h3>
+            <p>Gerencie Galerias existentes no site</p>
+        </div>
+    </div>
+</section>
+</template>
+
+<style scoped>
+    @reference '../../styles/global.css';
+    .dash-card{
+        @apply card-style flex flex-col;
+        p{
+            @apply text-text-muted
+        }
+        h3{
+            @apply text-xl flex-col font-bold
+        }
+    }
+</style>
