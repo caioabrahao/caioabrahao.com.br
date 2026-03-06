56f1873d7fdc201106c456d0104447ab76369795
Caio Abrahão <contato.caioabrahao@gmail.com>
2026-03-06 10:44:58 -0300

    feat: Add FileManager component and update navigation link in HeaderDash

---
+++ src/components/Dashboard/views/FileManager.vue
@@ -0,0 +1,11 @@
+<script setup>
+import BucketObjectList from '../BucketObjectList.vue';
+</script>
+
+<template>
+    <section class="global-padding-x pb-8">
+        <div class=" card-style-darker h-screen w-full" id="fileViewer">
+            <BucketObjectList/>
+        </div>
+    </section>
+</template>
