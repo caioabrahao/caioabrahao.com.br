19633f1fc8fe503dbb540a6a9bb1b84925a9820d
Caio Abrahão <contato.caioabrahao@gmail.com>
2026-03-09 16:29:02 -0300

    feat: Implemented basic Blog with dynamic routing and markdown style sheet

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+// src/content/config.ts
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content', 
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  'blog': blogCollection,
+};
